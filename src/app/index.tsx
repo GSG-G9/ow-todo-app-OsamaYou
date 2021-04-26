b4c601a44a30d6811b14df73ce6dbacc9e1096f3
Osama-you <osama.w.younis@gmail.com>
2021-04-27 00:48:29 +0300

    add react-helmet to the app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,12 @@
+import { Helmet } from 'react-helmet';
+
+import config from '../config';
+
 import styles from './styles.module.css';
 
 const App = (): JSX.Element => (
   <div className={styles.App}>
+    <Helmet {...config.APP} />
     App
   </div>
 );
