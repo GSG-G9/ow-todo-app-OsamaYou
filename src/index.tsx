00d222acacabf1ecb6644228be0f693c030f41d8
Osama-you <osama.w.younis@gmail.com>
2021-04-27 01:01:20 +0300

    setup redix store files Relates #2

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RouteConfig, renderRoutes } from 'react-router-config';
 
 import routes from './routes';
+import createStore from './store';
+
+const { store } = createStore();
 
 const renderMethod = ReactDOM.render;
 const render = (Routes: RouteConfig[]) => renderMethod(
-  <Router>
-    {renderRoutes(Routes)}
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      {renderRoutes(Routes)}
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
 
