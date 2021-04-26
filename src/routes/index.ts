4cac85c8a9edbb30ebdf346b97c162d0f0838a2d
Osama-you <osama.w.younis@gmail.com>
2021-04-27 01:19:41 +0300

    add home route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,17 @@ import { RouteConfig } from 'react-router-config';
 
 import App from '../app';
 import NotFound from '../pages/NotFound';
+import Home from '../pages/Home';
 
 export default [
   {
     component: App,
     routes: [
+      {
+        path: '/',
+        exact: true,
+        component: Home,
+      },
       {
         component: NotFound,
       },
