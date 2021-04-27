5c70aa608e2c1247f5b4293046fe150e6b939eeb
Osama-you <osama.w.younis@gmail.com>
2021-04-28 00:42:46 +0300

    add rootReducer and export same app type

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, configureStore } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
+
+import rootReducer from './rootReducer';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const createStore = () => {
   const store = configureStore({
-    reducer: {},
+    reducer: rootReducer,
   });
 
   return { store };
 };
+const { store } = createStore();
+
+export type AppState = ReturnType<typeof store.getState>;
+
+export type AppThunk = ThunkAction<void, AppState, unknown, Action<string>>;
 
 export default createStore;
