5dbd782d6f8f0df0e3fb2ec8ecbc8f9606d7efcd
Osama-you <osama.w.younis@gmail.com>
2021-04-27 18:00:58 +0300

    useing Header, MainContener & Footer inside Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,14 +2,21 @@ import { FC, memo } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
+import { Header, MainContener, Footer } from '../../components';
+
 import styles from './styles.module.css';
 
 export type Props = RouteComponentProps;
 
 const Home: FC<Props> = (): JSX.Element => (
-  <div className={styles.Home}>
+  <div className={styles.Bg}>
     <Helmet title="Home" />
-    Home
+    <div className={styles.Contener}>
+      <Header />
+      <MainContener />
+      <Footer />
+    </div>
+
   </div>
 );
 
