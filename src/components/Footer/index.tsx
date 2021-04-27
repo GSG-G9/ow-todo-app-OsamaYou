6404b97ce3b45695f17396de839ceaf688349d35
Osama-you <osama.w.younis@gmail.com>
2021-04-27 17:53:38 +0300

    create footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+const Footer = (): JSX.Element => (
+  <footer className={styles.Footer}>Drag and drop to reorder list</footer>
+);
+
+export default Footer;
