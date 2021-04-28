24543789398e4ed012aca501b9d796ca37925f08
Osama-you <osama.w.younis@gmail.com>
2021-04-28 14:40:40 +0300

    dispatch filter type Relates #14

--- src/components/MainContener/ToDoAction/index.tsx
+++ src/components/MainContener/ToDoAction/index.tsx
@@ -1,17 +1,41 @@
+import { useState } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../../../store';
+
+import { UpdateFilterIfNeed } from '../../../store/toDoList';
+
 import styles from './styles.module.css';
 
-const ToDoAction = (): JSX.Element => (
-  <div className={styles.ToDoAction}>
-    <span>5 items left</span>
-    <div className={styles.ToDoAction__Center}>
-      <button type="button">All</button>
-      <button type="button">Active</button>
-      <button type="button">Completed</button>
+const ToDoAction = (): JSX.Element => {
+  const [activeBtn, setActiveBtn] = useState({ all: true, active: false, completed: false });
+  const dispatch = useDispatch();
+  const { items } = useSelector(
+    ({ toDoList }: AppState) => toDoList,
+    shallowEqual,
+  );
+
+  const onActionClicked = (action: string) => () => {
+    dispatch(UpdateFilterIfNeed(action));
+    setActiveBtn({
+      all: false, active: false, completed: false, [action]: true,
+    });
+  };
+
+  return (
+    <div className={styles.ToDoAction}>
+      <span>
+        {`${items.length} items left`}
+      </span>
+      <div className={styles.ToDoAction__Center}>
+        <button className={activeBtn.all ? styles.ButtonClicked : ''} type="button" onClick={onActionClicked('all')} disabled={activeBtn.all}>All</button>
+        <button className={activeBtn.active ? styles.ButtonClicked : ''} type="button" onClick={onActionClicked('active')} disabled={activeBtn.active}>Active</button>
+        <button className={activeBtn.completed ? styles.ButtonClicked : ''} type="button" onClick={onActionClicked('completed')} disabled={activeBtn.completed}>Completed</button>
+      </div>
+      <button type="button">
+        Clear Completed
+      </button>
     </div>
-    <button type="button">
-      Clear Completed
-    </button>
-  </div>
-);
+  );
+};
 
 export default ToDoAction;
